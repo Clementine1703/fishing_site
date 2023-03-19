af41a80003ba5f21310cb923c7cecf73f974232f
Clementine1703 <kamil.alimetov@mail.ru>
2023-03-20 02:11:05 +0300

    -

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,12 +1,14 @@
 import sqlite3
 from flask import request
 from app import app
+from flask_cors import cross_origin
 import json
 
 connection = sqlite3.connect('data.db', check_same_thread=False)
 cursor = connection.cursor()
 
 @app.route('/postdata', methods=['POST'])
+@cross_origin()
 def postdata():
     username = request.form.get('username')
     password = request.form.get('password')
@@ -19,6 +21,7 @@ def postdata():
     return 'Отправьте необходимые данные!'
 
 @app.route('/getdata')
+@cross_origin()
 def getdata():
     token = request.headers.get('token')
 
